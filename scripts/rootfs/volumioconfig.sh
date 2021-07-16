fa5a69361389ba41ca5dddb26f828eba14b144c0
ashthespy <ashthespy@users.noreply.github.com>
2021-07-16 16:59:55 +0200

    Tweak console welcome

--- scripts/rootfs/volumioconfig.sh
+++ scripts/rootfs/volumioconfig.sh
@@ -333,8 +333,11 @@ systemctl disable ssh.service
 log "Enable Volumio SSH enabler"
 ln -s /lib/systemd/system/volumiossh.service /etc/systemd/system/multi-user.target.wants/volumiossh.service
 
-log "Enable Volumio Log Rotation Service"
-ln -s /lib/systemd/system/volumiologrotate.service /etc/systemd/system/multi-user.target.wants/volumiologrotate.service
+# log "Enable Volumio Log Rotation Service"
+# ln -s /lib/systemd/system/volumiologrotate.service /etc/systemd/system/multi-user.target.wants/volumiologrotate.service
+
+log "Enable Volumio Welcome Service"
+ln -s /lib/systemd/system/welcome.service /etc/systemd/system/multi-user.target.wants/welcome.service
 
 log "Setting Mpd to SystemD instead of Init"
 update-rc.d mpd remove
